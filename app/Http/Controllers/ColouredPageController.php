2a50be960b23d757e85a06685dfb35aca9138145
Ivan Yankov <ivan.yankov@rubberduck.xyz>
2020-03-08 15:28:02 +0200

    Create the needed controllers

---
+++ app/Http/Controllers/ColouredPageController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class ColouredPageController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return void
+     */
+    public function index()
+    {
+        //
+    }
+}
