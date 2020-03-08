0e198728e4fe7378af2af17a24d1bdd0862c8cc4
Ivan Yankov <ivan.yankov@rubberduck.xyz>
2020-03-08 16:27:33 +0200

    Writes a function to pass the colour from the API request

--- app/Http/Controllers/ColouredPageController.php
+++ app/Http/Controllers/ColouredPageController.php
@@ -1,16 +1,31 @@
 <?php
 
 namespace App\Http\Controllers;
+use GuzzleHttp\Psr7;
+use GuzzleHttp\Exception\ServerException;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\View\View;
 
 class ColouredPageController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Pass a colour to the view
      *
-     * @return void
+     * @return Factory|View
      */
     public function index()
     {
-        //
+        try {
+            $client = new \GuzzleHttp\Client(['base_uri' => 'https://go.trustvet.com/api/']);
+            $request = $client->request('GET', 'test');
+            $response = json_decode($request->getBody()->getContents());
+            $colour = $response->colour;
+        } catch (ServerException $exception) {
+            if ($exception->hasResponse()) {
+                $colour = '#ffffff';
+            }
+        }
+
+        return View('pages.coloured_page.index', compact('colour'));
     }
 }
